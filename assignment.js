53039599e47e5a4020877e3601627eb69c5e09aa
Nur A Alam <nuraalam376@gmail.com>
2020-07-17 03:59:18 +0600

    Implemented woodCalculator

--- assignment.js
+++ assignment.js
@@ -10,3 +10,23 @@ function feetToMile(feet) {
 		return mile;
 	}
 }
+
+//Calculates the total wood required for making the chair, table and bed
+function woodCalculator(chair, table, bed) {
+	// If any of the inputs is negative or not numbers, then return the error message
+	if (chair < 0 || table < 0 || bed < 0) {
+		return "Inputs cannot be negative. Please recheck the inputs.";
+	} else if (
+		typeof chair != "number" ||
+		typeof table != "number" ||
+		typeof bed != "number"
+	) {
+		return "Inputs must be numbers.Please recheck the inputs";
+	} else {
+		var chairCount = chair * 1; // 1 chair = 1 wood;
+		var tableCount = table * 3; // 1 table = 3 wood
+		var bedCount = bed * 5; // 1 bed = 5 bed
+		var totalWood = chairCount + tableCount + bedCount; // calculates the total wood
+		return totalWood;
+	}
+}
